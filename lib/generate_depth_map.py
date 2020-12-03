995b3f977ff7c862f2d9492a0f46081be9004259
zhaoguangyuan123 <zhaoguagnyuan@ucla.edu>
2020-12-03 06:51:22 +0000

    pts->depth map

--- lib/generate_depth_map.py
+++ lib/generate_depth_map.py
@@ -12,14 +12,27 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 def generate_dispariy_from_velo(pc_velo, height, width, calib):
     pts_2d = calib.project_velo_to_image(pc_velo)
+    print('pts_2d', pts_2d.shape)
+    # print('width', width)
+
     fov_inds = (pts_2d[:, 0] < width - 1) & (pts_2d[:, 0] >= 0) & \
                (pts_2d[:, 1] < height - 1) & (pts_2d[:, 1] >= 0)
+
+    print('fov_inds', fov_inds.shape)
+    print('pc_velo[1, 0]', pc_velo[1, 0])
+    print('pts_2d[1, 1]', pts_2d[1, 1])
+
     fov_inds = fov_inds & (pc_velo[:, 0] > 2)
+    print('valid_points', np.sum(fov_inds))
     imgfov_pc_velo = pc_velo[fov_inds, :]
     imgfov_pts_2d = pts_2d[fov_inds, :]
     imgfov_pc_rect = calib.project_velo_to_rect(imgfov_pc_velo)
     depth_map = np.zeros((height, width)) - 1
     imgfov_pts_2d = np.round(imgfov_pts_2d).astype(int)
+    print('imgfov_pts_2d.shape', imgfov_pts_2d.shape)
+    print('imgfov_pts_2d', imgfov_pts_2d[1])
+    print('imgfov_pc_velo', imgfov_pc_velo[1])
+
     for i in range(imgfov_pts_2d.shape[0]):
         depth = imgfov_pc_rect[i, 2]
         depth_map[int(imgfov_pts_2d[i, 1]), int(imgfov_pts_2d[i, 0])] = depth
@@ -56,6 +69,8 @@ if __name__ == '__main__':
         file_names = [x.strip() for x in f.readlines()]
 
     print('file_names', file_names[0])
+
+    i = 0
     for fn in lidar_files:
         predix = fn[:-4]
         if predix not in file_names:
@@ -67,13 +82,22 @@ if __name__ == '__main__':
         # load point cloud
         lidar = np.fromfile(lidar_dir + '/' + fn,
                             dtype=np.float32).reshape((-1, 4))[:, :3]
+        print('lidar', lidar.shape)
+
         image_file = '{}/{}.png'.format(image_dir, predix)
         image = imageio.imread(image_file)
+        print('image_shape', image.shape)
+
         height, width = image.shape[:2]
+
         depth_map = generate_dispariy_from_velo(lidar, height, width, calib)
         # print('depth_dir', depth_dir)
-        # print('depth_dir', depth_map.shape)
+        print('depth_dir', depth_map.shape)
+        print(depth_map)
         imageio.imwrite(depth_dir + '/' + predix+'.png', depth_map)
         np.save(depth_dir + '/' + predix, depth_map)
         print('Finish Depth Map {}'.format(predix))
-        print(ccc)
+
+        i = i+1
+        if i>1:
+            print(ccc)
